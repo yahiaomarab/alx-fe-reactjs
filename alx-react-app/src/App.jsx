097c90c341cc114a167aad09b44d22ae63fd6305
yahiaomarab <yahiaomar746@gmail.com>
2025-02-16 16:13:55 +0200

    Learn to create and assemble multiple specific React components within a main application component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header />
+
     <WelcomeMessage />
+    <MainContent/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,6 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer/>
     </>
   )
 }
---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+}
