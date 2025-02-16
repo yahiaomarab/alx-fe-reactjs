5cd38079f3d16f718285bd8edf3116009d36558a
yahiaomarab <yahiaomar746@gmail.com>
2025-02-16 16:03:16 +0200

    Familiarize yourself with JSX

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import WelcomeMessage from './component/WelcomeMessage'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
