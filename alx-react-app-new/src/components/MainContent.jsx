87ea70941d461b7002bf76b774e5c5cb435da7dd
yahiaomarab <yahiaomar746@gmail.com>
2025-02-23 18:57:01 +0200

    task-0

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <main style={{backgroundColor:'yellow'}}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+
+export default MainContent;
