2ff4df8fe24cc668067908370bba7513f0eda782
yahiaomarab <yahiaomar746@gmail.com>
2025-02-23 22:13:29 +0200

    task-2

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <nav style={{ backgroundColor: "#333", padding: "10px" }}>
+      <ul style={{ 
+        display: "flex", 
+        justifyContent: "space-around", 
+        listStyle: "none", 
+        padding: 0, 
+        margin: 0 
+      }}>
+        <li><Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link></li>
+        <li><Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link></li>
+        <li><Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link></li>
+        <li><Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link></li>
       </ul>
     </nav>
   );
