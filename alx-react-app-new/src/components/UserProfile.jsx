a3e1084dba3c1f3f0747f775d84425dbac7831ec
yahiaomarab <yahiaomar746@gmail.com>
2025-02-23 18:59:40 +0200

    task-0

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,10 @@
-const UserProfile = (props)=>{
-    return(
+const UserProfile = (props) => {
+    return (
         <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
             <h2 style={{ color: 'blue' }}>{props.name}</h2>
             <p style={{ fontWeight: 'bold' }}>Age: {props.age}</p>
             <p>Bio: {props.bio}</p>
+            <span style={{ color: 'green', fontStyle: 'italic' }}>Additional Info</span>
         </div>
     );
 }
