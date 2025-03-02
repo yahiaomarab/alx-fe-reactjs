335b62dd160b71bd8cea55f0f68a934b5aaee703
yahiaomarab <yahiaomar746@gmail.com>
2025-03-02 21:31:16 +0200

    building a Recipe Sharing Application with React and Zustand, setting up the initial state management for recipes.

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
+        
       </Routes>
     </Router>
   );
