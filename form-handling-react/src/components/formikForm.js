0b65c39f3474a5f056b4c8f4b779f2fa46be1a07
yahiaomarab <yahiaomar746@gmail.com>
2025-03-12 04:05:42 +0200

    ..

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -2,7 +2,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
-  username: Yup.string().min(3, "Must be at least 3 characters").required("Username is required"),
+  username: Yup.string().required("Username is required"),
   email: Yup.string().email("Invalid email format").required("Email is required"),
   password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
 });
