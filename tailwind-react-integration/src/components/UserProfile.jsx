44ef83043cca5e45234e3a34ab5e8534ae35cff8
yahiaomarab <yahia@verofax.com>
2025-03-16 21:50:34 +0200

    ...

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,7 +8,7 @@ function UserProfile() {
         alt="User"
         className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
       />
-      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-3 transition-colors duration-300 ease-in-out hover:text-blue-500">
+      <h1 className=" my-4 text-lg sm:text-lg md:text-xl text-blue-800 my-3 transition-colors duration-300 ease-in-out hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-sm sm:text-sm md:text-base text-gray-600 px-2">
