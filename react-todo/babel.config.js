1cf4564ebba86fbcc3d0f267f7fe571640072fef
yahiaomarab <yahia@verofax.com>
2025-03-16 21:22:59 +0200

    task -0

--- react-todo/babel.config.js
+++ react-todo/babel.config.js
@@ -1,4 +1,6 @@
-export const presets = [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-react',
-];
+module.exports = {
+    presets: [
+      ['@babel/preset-env', { targets: { node: 'current' } }],
+      '@babel/preset-react',
+    ],
+  };
